4ab0127f9ad5a38105a22e697f43619073eed4d1
Julian Waller <git@julusian.co.uk>
2018-10-31 23:30:05 +0000

    fix: Add missing iterateDeeplyAsync

--- src/util.ts
+++ src/util.ts
@@ -26,6 +26,27 @@ export function iterateDeeply (obj: any, iteratee: (val: any, key?: string | num
 		return newValue
 	}
 }
+/**
+ * Iterates deeply through object or array, using an asynchronous iteratee
+ * @param obj the object or array to iterate through
+ * @param iteratee function to apply on every attribute
+ */
+export async function iterateDeeplyAsync (obj: any, iteratee: (val: any, key?: string | number) => Promise<any | iterateDeeplyEnum>, key?: string | number) {
+	let newValue = await iteratee(obj, key)
+	if (newValue === iterateDeeplyEnum.CONTINUE) {
+		// Continue iterate deeper if possible
+		if (_.isObject(obj)) { // object or array
+			await Promise.all(_.map(obj, async (val, key) => {
+				obj[key] = await iterateDeeply(val, iteratee, key)
+			}))
+		} else {
+			// don't change anything
+		}
+		return obj
+	} else {
+		return newValue
+	}
+}
 
 export enum iterateDeeplyEnum {
 	CONTINUE = '$continue'
