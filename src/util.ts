dba6e7723c3ff7991dac630c6c880bbe0a5fbb2d
Julian Waller <git@julusian.co.uk>
2018-11-05 19:38:14 +0000

    fix: Remove getHash as it leaves a require in the built blob

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,5 @@
 import * as _ from 'underscore'
-import * as crypto from 'crypto'
 
-export function getHash (str: string): string {
-	const hash = crypto.createHash('sha1')
-	return hash.update(str).digest('base64').replace(/[\+\/\=]/g, '_') // remove +/= from strings, because they cause troubles
-}
 /**
  * Iterates deeply through object or array
  * @param obj the object or array to iterate through
