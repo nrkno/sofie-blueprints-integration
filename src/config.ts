cf9a68b92c4e604d697f591efd241b8d98f85b61
Johan Nyman <johan@nytamin.se>
2018-11-20 09:39:48 +0100

    fix: reverting splitting configManifest

--- src/config.ts
+++ src/config.ts
@@ -4,17 +4,11 @@ export enum ConfigManifestEntryType {
 	NUMBER = 'number',
 	BOOLEAN = 'boolean'
 }
-export enum ConfigManifestEntrySource {
-	STUDIO = 'studio',
-	SHOWSTYLE = 'showstyle'
-}
-
 export interface ConfigManifestEntry {
 	id: string
 	name: string
 	description: string
 	type: ConfigManifestEntryType
-	source: ConfigManifestEntrySource
 	required: boolean,
 	defaultVal: number | string | boolean // TODO - type this?
 }
