5ba3fd30a9217d9b171ec47b95d0560cb491ca68
Julian Waller <git@julusian.co.uk>
2019-04-09 12:02:46 +0100

    chore: Remove mos typings

--- src/copy/mos-connection/Utils.ts
+++
@@ -1,6 +0,0 @@
-/** */
-export function pad (n: string, width: number, z?: string): string {
-	z = z || '0'
-	n = n + ''
-	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
-}
