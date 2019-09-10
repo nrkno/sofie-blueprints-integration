3c96a2cbb6fbf7e32532bc84abcda5bff2c7c438
Julian Waller <git@julusian.co.uk>
2019-09-10 10:47:18 +0100

    fix: move metaData field to TimelineObjectCoreExt instead of being defined within the blueprint typings

--- src/timeline.ts
+++ src/timeline.ts
@@ -42,6 +42,8 @@ export enum TimelineObjHoldMode {
 export interface TimelineObjectCoreExt extends TSRTimelineObjBase {
 	/** Restrict object usage according to whether we are currently in a hold */
 	holdMode?: TimelineObjHoldMode
+	/** Arbitrary data storage for plugins */
+	metaData?: { [key: string]: any }
 }
 
 /** TimelineObject extension for additional fields needed by onTimelineGenerate */
