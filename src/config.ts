2214d267b0f264f3f0c15330d48612385bb08050
Tom Lee <thomasslee97@gmail.com>
2020-04-14 11:38:48 +0100

    feat: Table column ranks
    (cherrypicked from 42f5293fef70640bab52e9246832a26e30639918)

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,12 @@ export interface ConfigManifestEntryEnum extends ConfigManifestEntryBase {
 }
 export interface ConfigManifestEntryTable extends ConfigManifestEntryBase {
 	type: ConfigManifestEntryType.TABLE
-	columns: BasicConfigManifestEntry[]
+	columns: Array<
+		BasicConfigManifestEntry & {
+			/** Column rank (left to right, lowest to highest) */
+			rank: number
+		}
+	>
 	defaultVal: TableConfigItemValue
 }
 
