f53159b46f33a6ccb75c33718a84c31240d6660b
ianshade <zegzulakrzysztof@gmail.com>
2020-08-20 18:06:08 +0200

    refactor: blueprint config type

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,10 @@
 export type Time = number
 
-export interface IConfigItem {
-	_id: string
-	value: ConfigItemValue
+export interface IBlueprintConfig {
+	[key: string]: ConfigItemValue
 }
 
-export type ConfigItemValue = BasicConfigItemValue | TableConfigItemValue
+export type ConfigItemValue = BasicConfigItemValue | TableConfigItemValue | IBlueprintConfig
 export type TableConfigItemValue = {
 	_id: string
 	[key: string]: BasicConfigItemValue
